55f8deb4a19aa521d37dd01c1b27c1089dbd732d
Veuz Concepts <veuzconcepts@Veuzs-MacBook-Pro.local>
2023-12-06 13:05:08 +0530

    2nd update

--- contact.php
+++ contact.php
@@ -27,75 +27,10 @@
   <!-- Preloader -->
   <div class="inner-banner">
     <div class="banner-overlay">
-      <div class="nav-box">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="d-flex justify-content-between w-100">
-            <div>
-              <a class="navbar-brand" href="#">
-                <img src="img/tss-logo.png" alt="">
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-            </div>
-
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-
-
-              <ul class="navbar-nav nav-links">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="about.html">About us</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="service.html">Services</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="portfolio.html">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="blog.html">Blog</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="career.html">Career</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Contact</a>
-                </li>
-              </ul>
-              <ul class="nav-section-2 nav-links d-flex">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    UAE
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <li><a class="dropdown-item" href="#">USA</a></li>
-                    <li><a class="dropdown-item" href="#">IND</a></li>
-                  </ul>
-                </li>
-                <li class="d-flex align-items-center">
-                  <a href="">
-                    <img src="img/earth-icon.svg" alt="" class="img-fluid globe-icon">
-                    عربى
-                  </a>
-                </li>
-              </ul>
-              <button type="button" class="main-btn">
-                Business Enquiry
-              </button>
-
-            </div>
-          </div>
-
-        </nav>
-      </div>
-      <div class="d-flex justify-content-center align-items-center flex-column">
+     <?php include 'header.php';?>
+      <div class="inner-banner-center">
         <h2 class="banner-head">Feel Free to Contact Us</h2>
-        <div class="d-flex">
+        <div class="d-flex justify-content-center aos-init aos-animate" data-aos="zoom-in" data-aos-duration="3000">
           <button type="button" class="main-home-btn">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
               <g clip-path="url(#clip0_402_13827)">
---
+++ header.php
@@ -0,0 +1,70 @@
+<div class="nav-box">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="d-flex justify-content-between w-100">
+            <div>
+              <a class="navbar-brand" href="#">
+                <img src="img/tss-logo.png" alt="">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            </div>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+
+
+              <ul class="navbar-nav nav-links">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.php">About us</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="service.php">Services</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="portfolio.php">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="blog.php">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="career.php">Career</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+              </ul>
+              <ul class="nav-section-2 nav-links d-flex">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    UAE
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">USA</a></li>
+                    <li><a class="dropdown-item" href="#">IND</a></li>
+                  </ul>
+                </li>
+                <li class="d-flex align-items-center">
+                  <a href="">
+                    <img src="img/earth-icon.svg" alt="" class="img-fluid globe-icon">
+                    عربى
+                  </a>
+                </li>
+              </ul>
+              <button type="button" class="main-btn">
+                Business Enquiry
+              </button>
+
+            </div>
+          </div>
+
+        </nav>
+      </div>
+
+      
+
+
