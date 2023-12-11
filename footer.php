f4109dfd4ae9ec6712f1842a92f58e3224a26939
anakha <anakha@veuz.in>
2023-12-11 17:34:52 +0530

    event detail and footer

--- footer.php
+++ footer.php
@@ -1,116 +1,164 @@
 <footer>
-    <div class="container">
-      <div class="footer-top" data-aos="fade-up" data-aos-duration="2000">
-        <button type="button" class="btn4">
-          <span>Business Enquiry</span> 
-        </button>
-        <div class="input-group">
-          <input type="email" class="form-control" placeholder="Subscribe to our newsletter">
-          <button type="submit" class="submit-btn"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.1097 13.6502L13.6897 10.0602M7.39969 6.32015L15.8897 3.49015C19.6997 2.22015 21.7697 4.30015 20.5097 8.11015L17.6797 16.6002C15.7797 22.3102 12.6597 22.3102 10.7597 16.6002L9.91969 14.0802L7.39969 13.2402C1.68969 11.3402 1.68969 8.23015 7.39969 6.32015Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            </button>
-        </div>
-        <ul class="nav-section-2 nav-links d-flex">
-          <li class="d-flex align-items-center">
-            <a  href="" class="footer-arabic-cntnt">
-              <img src="img/earth-icon.svg" alt="" class="img-fluid globe-icon-footer">
-              عربى
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              UAE
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="#">USA</a></li>
-              <li><a class="dropdown-item" href="#">IND</a></li>
-            </ul>
-          </li>
-         
-        </ul>
+  <div class="container">
+    <div class="footer-top" data-aos="fade-up" data-aos-duration="2000">
+      <button type="button" class="btn4">
+        <span>Business Enquiry</span> 
+      </button>
+      <div class="input-group">
+        <input type="email" class="form-control" placeholder="Subscribe to our newsletter">
+        <button type="submit" class="submit-btn"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M10.1097 13.6502L13.6897 10.0602M7.39969 6.32015L15.8897 3.49015C19.6997 2.22015 21.7697 4.30015 20.5097 8.11015L17.6797 16.6002C15.7797 22.3102 12.6597 22.3102 10.7597 16.6002L9.91969 14.0802L7.39969 13.2402C1.68969 11.3402 1.68969 8.23015 7.39969 6.32015Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          </button>
       </div>
-      <div class="footer-middle" data-aos="fade-up" data-aos-duration="2000">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="footer-about">
-              <img src="img/tss-logo.png" alt="" class="footer-logo">
-              <p>TSS Advertising Company was established in the year 2008 with a comprehensive and an extensive vision to cater the needs of the advertising industry in the Middle East.</p>
-              <div class="footer-contact">
-                <ul class="footer-left-contact">
-                  <li><a href="mailto:info@tss-adv.com"><svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_1_660)">
-                    <g opacity="0.5">
-                    <path d="M0 4C0 1.79086 1.79086 0 4 0H16C18.2091 0 20 1.79086 20 4V14C20 16.2091 18.2091 18 16 18H4C1.79086 18 0 16.2091 0 14V4Z" fill="white"/>
-                    <mask id="mask0_1_660" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="20" height="18">
-                    <path d="M0 4C0 1.79086 1.79086 0 4 0H16C18.2091 0 20 1.79086 20 4V14C20 16.2091 18.2091 18 16 18H4C1.79086 18 0 16.2091 0 14V4Z" fill="white"/>
-                    </mask>
-                    <g mask="url(#mask0_1_660)">
-                    <path d="M0 0L7.78018 8.5582C8.97046 9.8675 11.0295 9.86751 12.2198 8.5582L20 0" stroke="#111111" stroke-width="2"/>
-                    </g>
-                    </g>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_1_660">
-                    <rect width="20" height="18" fill="white"/>
-                    </clipPath>
-                    </defs>
-                    </svg> Email</a> 
-                    </li>
-                  <li><a href="tel:+966920000619"><svg width="13" height="18" viewBox="0 0 13 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clip-path="url(#clip0_1_667)">
-                    <g opacity="0.5">
-                    <path d="M7 0H6C5.44772 0 5 0.447715 5 1V2C5 2.55228 5.44772 3 6 3H7C7.55228 3 8 2.55228 8 2V1C8 0.447715 7.55228 0 7 0Z" fill="white"/>
-                    <path d="M2 0H1C0.447715 0 0 0.447715 0 1V2C0 2.55228 0.447715 3 1 3H2C2.55228 3 3 2.55228 3 2V1C3 0.447715 2.55228 0 2 0Z" fill="white"/>
-                    <path d="M12 0H11C10.4477 0 10 0.447715 10 1V2C10 2.55228 10.4477 3 11 3H12C12.5523 3 13 2.55228 13 2V1C13 0.447715 12.5523 0 12 0Z" fill="white"/>
-                    <path d="M7 5H6C5.44772 5 5 5.44772 5 6V7C5 7.55228 5.44772 8 6 8H7C7.55228 8 8 7.55228 8 7V6C8 5.44772 7.55228 5 7 5Z" fill="white"/>
-                    <path d="M2 5H1C0.447715 5 0 5.44772 0 6V7C0 7.55228 0.447715 8 1 8H2C2.55228 8 3 7.55228 3 7V6C3 5.44772 2.55228 5 2 5Z" fill="white"/>
-                    <path d="M12 5H11C10.4477 5 10 5.44772 10 6V7C10 7.55228 10.4477 8 11 8H12C12.5523 8 13 7.55228 13 7V6C13 5.44772 12.5523 5 12 5Z" fill="white"/>
-                    <path d="M7 10H6C5.44772 10 5 10.4477 5 11V12C5 12.5523 5.44772 13 6 13H7C7.55228 13 8 12.5523 8 12V11C8 10.4477 7.55228 10 7 10Z" fill="white"/>
-                    <path d="M2 10H1C0.447715 10 0 10.4477 0 11V12C0 12.5523 0.447715 13 1 13H2C2.55228 13 3 12.5523 3 12V11C3 10.4477 2.55228 10 2 10Z" fill="white"/>
-                    <path d="M12 10H11C10.4477 10 10 10.4477 10 11V12C10 12.5523 10.4477 13 11 13H12C12.5523 13 13 12.5523 13 12V11C13 10.4477 12.5523 10 12 10Z" fill="white"/>
-                    <path d="M7 15H6C5.44772 15 5 15.4477 5 16V17C5 17.5523 5.44772 18 6 18H7C7.55228 18 8 17.5523 8 17V16C8 15.4477 7.55228 15 7 15Z" fill="white"/>
-                    </g>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_1_667">
-                    <rect width="13" height="18" fill="white"/>
-                    </clipPath>
-                    </defs>
-                    </svg>Phone</a>
-                    </li>
-                </ul>
-                <ul class="footer-right-contact">
-                  <li><a href="mailto:info@tss-adv.com">info@tss-adv.com</a></li>
-                  <li><a href="tel:+966920000619">+966 920000619</a></li>
-                </ul>
-               
+      <ul class="nav-section-2 nav-links d-flex">
+        <li class="d-flex align-items-center">
+          <a  href="" class="footer-arabic-cntnt">
+            <img src="img/earth-icon.svg" alt="" class="img-fluid globe-icon-footer">
+            عربى
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            UAE
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">USA</a></li>
+            <li><a class="dropdown-item" href="#">IND</a></li>
+          </ul>
+        </li>
+       
+      </ul>
+    </div>
+    <div class="footer-middle" data-aos="fade-up" data-aos-duration="2000">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="footer-about">
+            <img src="img/tss-logo.png" alt="" class="footer-logo">
+            <p>TSS Advertising Company was established in the year 2008 with a comprehensive and an extensive vision to cater the needs of the advertising industry in the Middle East.</p>
+            <div class="footer-contact">
+              <ul class="footer-left-contact">
+                <li><a href="mailto:info@tss-adv.com"><svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_1_660)">
+                  <g opacity="0.5">
+                  <path d="M0 4C0 1.79086 1.79086 0 4 0H16C18.2091 0 20 1.79086 20 4V14C20 16.2091 18.2091 18 16 18H4C1.79086 18 0 16.2091 0 14V4Z" fill="white"/>
+                  <mask id="mask0_1_660" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="20" height="18">
+                  <path d="M0 4C0 1.79086 1.79086 0 4 0H16C18.2091 0 20 1.79086 20 4V14C20 16.2091 18.2091 18 16 18H4C1.79086 18 0 16.2091 0 14V4Z" fill="white"/>
+                  </mask>
+                  <g mask="url(#mask0_1_660)">
+                  <path d="M0 0L7.78018 8.5582C8.97046 9.8675 11.0295 9.86751 12.2198 8.5582L20 0" stroke="#111111" stroke-width="2"/>
+                  </g>
+                  </g>
+                  </g>
+                  <defs>
+                  <clipPath id="clip0_1_660">
+                  <rect width="20" height="18" fill="white"/>
+                  </clipPath>
+                  </defs>
+                  </svg> Email</a> 
+                  </li>
+                <li><a href="tel:+966920000619"><svg width="13" height="18" viewBox="0 0 13 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_1_667)">
+                  <g opacity="0.5">
+                  <path d="M7 0H6C5.44772 0 5 0.447715 5 1V2C5 2.55228 5.44772 3 6 3H7C7.55228 3 8 2.55228 8 2V1C8 0.447715 7.55228 0 7 0Z" fill="white"/>
+                  <path d="M2 0H1C0.447715 0 0 0.447715 0 1V2C0 2.55228 0.447715 3 1 3H2C2.55228 3 3 2.55228 3 2V1C3 0.447715 2.55228 0 2 0Z" fill="white"/>
+                  <path d="M12 0H11C10.4477 0 10 0.447715 10 1V2C10 2.55228 10.4477 3 11 3H12C12.5523 3 13 2.55228 13 2V1C13 0.447715 12.5523 0 12 0Z" fill="white"/>
+                  <path d="M7 5H6C5.44772 5 5 5.44772 5 6V7C5 7.55228 5.44772 8 6 8H7C7.55228 8 8 7.55228 8 7V6C8 5.44772 7.55228 5 7 5Z" fill="white"/>
+                  <path d="M2 5H1C0.447715 5 0 5.44772 0 6V7C0 7.55228 0.447715 8 1 8H2C2.55228 8 3 7.55228 3 7V6C3 5.44772 2.55228 5 2 5Z" fill="white"/>
+                  <path d="M12 5H11C10.4477 5 10 5.44772 10 6V7C10 7.55228 10.4477 8 11 8H12C12.5523 8 13 7.55228 13 7V6C13 5.44772 12.5523 5 12 5Z" fill="white"/>
+                  <path d="M7 10H6C5.44772 10 5 10.4477 5 11V12C5 12.5523 5.44772 13 6 13H7C7.55228 13 8 12.5523 8 12V11C8 10.4477 7.55228 10 7 10Z" fill="white"/>
+                  <path d="M2 10H1C0.447715 10 0 10.4477 0 11V12C0 12.5523 0.447715 13 1 13H2C2.55228 13 3 12.5523 3 12V11C3 10.4477 2.55228 10 2 10Z" fill="white"/>
+                  <path d="M12 10H11C10.4477 10 10 10.4477 10 11V12C10 12.5523 10.4477 13 11 13H12C12.5523 13 13 12.5523 13 12V11C13 10.4477 12.5523 10 12 10Z" fill="white"/>
+                  <path d="M7 15H6C5.44772 15 5 15.4477 5 16V17C5 17.5523 5.44772 18 6 18H7C7.55228 18 8 17.5523 8 17V16C8 15.4477 7.55228 15 7 15Z" fill="white"/>
+                  </g>
+                  </g>
+                  <defs>
+                  <clipPath id="clip0_1_667">
+                  <rect width="13" height="18" fill="white"/>
+                  </clipPath>
+                  </defs>
+                  </svg>Phone</a>
+                  </li>
+              </ul>
+              <ul class="footer-right-contact">
+                <li><a href="mailto:info@tss-adv.com">info@tss-adv.com</a></li>
+                <li><a href="tel:+966920000619">+966 920000619</a></li>
+              </ul>
+             
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="footer-link">
+            <h5>Quick Links</h5>
+            <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="about.php">About us</a></li>
+              <li><a href="service.php">Services</a></li>
+              <li><a href="blog.php">Blogs</a></li>
+              <li><a href="portfolio.php">Portfolio</a></li>
+              <li><a href="career.php">Careers</a></li>
+              <li><a href="contact.php">Contact us</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="footer-map">
+            <img src="img/footer-map.png" alt="" class="img-fluid">
+            <div class="egypt">
+              <div class="loc-tooltip">
+                <h6>Egypt</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
+              </div>
+            </div>
+            <div class="riyad">
+              <div class="loc-tooltip">
+                <h6>Riyad</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
+              </div>
+            </div>
+            <div class="oman">
+              <div class="loc-tooltip">
+                <h6>Oman</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
+              </div>
+            </div>
+            <div class="dammam">
+              <div class="loc-tooltip">
+                <h6>Dammam</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
+              </div>
+            </div>
+            <div class="bahrain">
+              <div class="loc-tooltip">
+                <h6>Bahrain</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
+              </div>
+            </div>
+            <div class="dubai">
+              <div class="loc-tooltip">
+                <h6>Dubai</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
+              </div>
+            </div>
+            <div class="qatar">
+              <div class="loc-tooltip">
+                <h6>Qatar</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
+              </div>
+            </div>
+            <div class="jeddah">
+              <div class="loc-tooltip">
+                <h6>Jeddah</h6>
+                <p>24 B El Zahraa St., New Nerco Bldgs, Maadi, Cairo, Egypt</p>
               </div>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="footer-link">
-              <h5>Quick Links</h5>
-              <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About us</a></li>
-                <li><a href="service.php">Services</a></li>
-                <li><a href="blog.php">Blogs</a></li>
-                <li><a href="portfolio.php">Portfolio</a></li>
-                <li><a href="career.php">Careers</a></li>
-                <li><a href="contact.php">Contact us</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md-5">
-            <div class="footer-map">
-              <img src="img/footer-map.png" alt="" class="img-fluid">
-            </div>
-          </div>
         </div>
       </div>
-      <div class="footer-bottom">
-        <p>© 2023 <a href="#">TSS Advertising Company</a>. All Rights Reserved.</p>
-      </div>
     </div>
-  </footer>
+    <div class="footer-bottom">
+      <p>© 2023 <a href="#">TSS Advertising Company</a>. All Rights Reserved.</p>
+    </div>
+  </div>
+</footer>
